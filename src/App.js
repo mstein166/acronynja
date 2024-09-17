7141d3b2ce3197c361d23b17084ee724fe2a2f3e
mstein166 <mstein166@gmail.com>
2024-09-16 21:05:27 -0700

    Ping reset

--- src/App.js
+++ src/App.js
@@ -213,7 +213,7 @@ const AcronymGuesser = () => {
   {showAnswerButton && !gameOver && (
   <div className="mb-4">
     <Button className="relative flex items-center justify-center" onClick={handleShowAnswer}>
-      <span className="absolute flex h-3 w-3">
+      <span className="absolute top-0 right-0 mt-1 mr-1 flex h-3 w-3">
         <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
         <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
       </span>
