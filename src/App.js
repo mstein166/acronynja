d5621f0cb8dcec3f44005dc7ea163851c4f064bc
mstein166 <mstein166@gmail.com>
2024-09-16 20:59:31 -0700

    Testing ping

--- src/App.js
+++ src/App.js
@@ -210,11 +210,18 @@ const AcronymGuesser = () => {
         </div>
       )}
 
-    {showAnswerButton && !gameOver && (
-        <div className="mb-4">
-          <Button onClick={handleShowAnswer}>Show me the answer!</Button>
-        </div>
-      )}
+  {showAnswerButton && !gameOver && (
+  <div className="mb-4">
+    <Button className="relative flex items-center justify-center" onClick={handleShowAnswer}>
+      <span className="absolute flex h-3 w-3">
+        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+        <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+      </span>
+      Show me the answer!
+    </Button>
+  </div>
+)}
+
 
       {message && (
         <Alert className="mb-4">
